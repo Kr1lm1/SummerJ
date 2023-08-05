3015ccc880450cc1276517c355556315702804ef
Kr1lm1 <136977752+Kr1lm1@users.noreply.github.com>
2023-08-05 21:49:34 +0500

    7rfujymhf

--- Assets/Scripts/PlayerController_Mode0.cs
+++ Assets/Scripts/PlayerController_Mode0.cs
@@ -2,23 +2,32 @@ using UnityEngine;
 
 public class PlayerController_Mode0 : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector2 direction;
-    public Animator Anim;
+    [SerializeField] private Animator Anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (gameObject.GetComponent<PlayerTransformation>().PlayerMode == 0)
         {
             direction.x = Input.GetAxisRaw("Horizontal");
             direction.y = Input.GetAxisRaw("Vertical");
-            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+
+            Anim.SetFloat("Horizontal", direction.x);
+            Anim.SetFloat("Vertical", direction.y);
+            Anim.SetFloat("Speed", direction.sqrMagnitude);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
     }
 }
