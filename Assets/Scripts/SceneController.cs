d3adf7726b90a7d33373f4411b186196dd1cac19
agarkovegorik <121577892+agarkovegorik@users.noreply.github.com>
2023-08-06 10:27:38 +0300

    7

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     [SerializeField] string Scene;
 
     public void ChangeScene()
     {
         SceneManager.LoadScene(Scene);
+        Time.timeScale = 1.0f;
     }
 }
