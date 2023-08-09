22ead303cb7d3ef738dfb4a923c628b67586de09
Kr1lm1 <136977752+Kr1lm1@users.noreply.github.com>
2023-08-09 14:51:13 +0500

    ьрсвьононоептрпр

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-
-
 
 public class TimeDisplay : MonoBehaviour
 {
-    public TextMeshProUGUI timeText;
+    public Text timeText;
 
     private float currentTime = 0f;
 
-    void Update()
+    void Start()
     {
-        // Получаем текущее время в секундах
-        currentTime = Time.time;
+        // Обнуляем таймер при запуске
+        currentTime = 0f;
+    }
+
+    void FixedUpdate()
+    {
+        // Увеличиваем текущее время в секундах
+        currentTime += Time.fixedDeltaTime;
 
         // Отображаем текущее время на текстовом элементе
-        timeText.text = "Время: " + currentTime.ToString("F2");
+        timeText.text = "Время: " + currentTime.ToString("F2"); // "F2" указывает, что выводим два знака после запятой
     }
 }
